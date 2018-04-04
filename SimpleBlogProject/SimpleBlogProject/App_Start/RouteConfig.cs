1f97d18e8ab3cba1180ed7146a009b1b88638185
Niyazi Ozturk <nozt001@aucklanduni.ac.nz>
2018-04-04 21:08:53 +1200

    Home (postscontroller), Auth controllers are added. Admin Area (namespace) created for admin functions, also two Users and Posts controllers added. The namespace class of PostsControllers solved by adding namespaces variable in routeconfig.cs as last param to MapRoute() method

--- SimpleBlogProject/SimpleBlogProject/App_Start/RouteConfig.cs
+++ SimpleBlogProject/SimpleBlogProject/App_Start/RouteConfig.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using SimpleBlogProject.Controllers;
 
 namespace SimpleBlogProject
 {
@@ -11,13 +13,21 @@ namespace SimpleBlogProject
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            var namespaces = new[] { typeof(PostsController).Namespace };
+
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute("Login", "login", new { controller = "Auth", action = "Login" }, namespaces);
+
+            routes.MapRoute("Home", "", new { controller = "Posts", action = "Index" }, namespaces);
+
+            /*
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            */
         }
     }
 }
